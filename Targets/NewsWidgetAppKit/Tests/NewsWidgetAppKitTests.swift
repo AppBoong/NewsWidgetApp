2ad042d5c3ae530f79367f228b54fe24c1d404cf
JaeYeol Lee <powerhotdog21@gmail.com>
2023-09-19 07:14:39 -0700

    Create Project

---
+++ Targets/NewsWidgetAppKit/Tests/NewsWidgetAppKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class NewsWidgetAppKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("NewsWidgetAppKit", "NewsWidgetAppKit")
+    }
+}
---
+++ Targets/NewsWidgetAppUI/Tests/NewsWidgetAppUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class NewsWidgetAppUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("NewsWidgetAppUI", "NewsWidgetAppUI")
+    }
+}
