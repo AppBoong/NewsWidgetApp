c76e45043d7b5076a9bb52d3b389b19607f74775
JaeYeol Lee <powerhotdog21@gmail.com>
2023-09-22 23:37:00 +0900

    Add APIConfig

---
+++ Targets/NewsWidgetApp/Sources/App/APIConfig.swift
@@ -0,0 +1,14 @@
+//
+//  APIConfig.swift
+//  NewsWidgetApp
+//
+//  Created by 이재열 on 2023/09/22.
+//  Copyright © 2023 com.Appboong. All rights reserved.
+//
+
+import Foundation
+
+struct APIConfig {
+    static let clientID = ProcessInfo.processInfo.environment["CLIENT_ID"]
+    static let token = ProcessInfo.processInfo.environment["CLIENT_SECRET"]
+}
