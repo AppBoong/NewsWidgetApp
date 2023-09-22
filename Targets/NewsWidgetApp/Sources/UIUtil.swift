ae6fa6f23561a92e805abb7c82628e024c4561eb
JaeYeol Lee <powerhotdog21@gmail.com>
2023-09-22 23:02:29 +0900

    Add UIUtil

---
+++ Targets/NewsWidgetApp/Sources/UIUtil.swift
@@ -0,0 +1,27 @@
+//
+//  UIUtil.swift
+//  NewsWidgetApp
+//
+//  Created by 이재열 on 2023/09/22.
+//  Copyright © 2023 com.Appboong. All rights reserved.
+//
+
+import SwiftUI
+
+public extension View {
+  @ViewBuilder
+  func _overlay(
+    alignment: Alignment = .center,
+    @ViewBuilder content: () -> some View
+  ) -> some View {
+      self.overlay(content(), alignment: alignment)
+  }
+  
+  @ViewBuilder
+  func _background(
+    alignment: Alignment = .center,
+    @ViewBuilder content: () -> some View
+  ) -> some View {
+      self.background(content(), alignment: alignment)
+  }
+}
