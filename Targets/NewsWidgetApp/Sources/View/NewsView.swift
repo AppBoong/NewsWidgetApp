2899c8d3b059d1c704adba5f41b452d334197beb
JaeYeol Lee <powerhotdog21@gmail.com>
2023-09-26 18:15:32 +0900

    Modify NewsView UI 수정

--- Targets/NewsWidgetApp/Sources/View/NewsView.swift
+++ Targets/NewsWidgetApp/Sources/View/NewsView.swift
@@ -17,31 +17,49 @@ struct NewsView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                SearchBar(placeholder: "검색어를 입력해주세요.",
-                          text: viewStore.binding(
-                            get: \.searchText,
-                            send: {.searchTextChanged($0)})
-                ) {
-                    print(viewStore.state.isLoading)
-                    viewStore.send(.requestSearch)
-                }
-                .padding(20)
-                
-                
-                if let news = viewStore.state.news {
-                    List(news.items) { item in
-                        VStack {
-                            Text(item.title)
-                            Text(item.description)
-                        }
+            ZStack {
+               
+                List(viewStore.newsList) { item in
+                   
+                    VStack {
+                        Spacer()
+                            .frame(height: 40)
+                        Text(item.title)
+                        Text(item.description.htmlToAttributedString()?.string ?? "")
                     }
                 }
                 
+                GeometryReader { geo in
+                    SearchBar(placeholder: "검색어를 입력해주세요.",
+                              text: viewStore.binding(
+                                get: \.searchText,
+                                send: {.searchTextChanged($0)})
+                    ) {
+                        print(viewStore.state.isLoading)
+                        viewStore.send(.requestSearch)
+                    }
+                    .position(x: (geo.size.width - 40) / 2, y: -5)
+                    .padding(20)
+                }
                 
-                Spacer(minLength: 20)
+                
             }
+            .navigationTitle("News")
         }
-        .navigationTitle("News")
+    }
+}
+
+extension String {
+    func htmlToAttributedString() -> NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [
+                .documentType: NSAttributedString.DocumentType.html,
+                .characterEncoding: String.Encoding.utf8.rawValue
+            ], documentAttributes: nil)
+        } catch {
+            print("Error parsing HTML: \(error.localizedDescription)")
+            return nil
+        }
     }
 }
